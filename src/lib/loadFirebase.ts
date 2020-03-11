95a6126865f5b7344c620110966aa61141f56f03
TemTem <t3mir1an@gmail.com>
2020-03-11 01:49:25 -0500

    setup firebase, ts, next

---
+++ src/lib/loadFirebase.ts
@@ -0,0 +1,25 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from 'firebase/app'
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+// import "firebase/analytics";
+
+// // Add the Firebase products that you want to use
+// import "firebase/auth";
+// import "firebase/firestore";
+
+export default () => {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyD2-NHlVpXhk25NDLp4AI744bh6cUhXDjc',
+    authDomain: 'ahoy-kino.firebaseapp.com',
+    databaseURL: 'https://ahoy-kino.firebaseio.com',
+    projectId: 'ahoy-kino',
+    storageBucket: 'ahoy-kino.appspot.com',
+    messagingSenderId: '286129637538',
+    appId: '1:286129637538:web:0ca508b13d1ab874ef2ffb',
+  }
+  firebase.initializeApp(firebaseConfig)
+  return firebase
+}
+
+// Initialize Firebase
