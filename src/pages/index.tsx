0503673e3e18b12cf4822a613bc405c3c03cfdc2
TemTem <t3mir1an@gmail.com>
2020-03-15 00:08:08 -0500

    get init props on main page, fix infinite scroller

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
 import Head from 'next/head'
 import Feed from '~/components/Feed'
+import { NextPage, NextPageContext } from 'next'
+import { Movie } from '~/types'
+import loadFirebase from '~/lib/loadFirebase'
 
-const Home = () => {
+interface Props {
+  pageMovies?: Movie[]
+  ELEMENTS_ON_PAGE: number
+}
+
+const Home: NextPage<Props> = props => {
+  const { pageMovies, ELEMENTS_ON_PAGE } = props
+
   return (
     <div className='container'>
       <Head>
@@ -10,10 +20,43 @@ const Home = () => {
       </Head>
 
       <main>
-        <Feed />
+        <Feed initMovies={pageMovies} ELEMENTS_ON_PAGE={ELEMENTS_ON_PAGE} />
       </main>
     </div>
   )
 }
+
+const getProps = () => async (props: NextPageContext) => {
+  const db = loadFirebase().firestore()
+  const ELEMENTS_ON_PAGE = 3
+
+  const page = db
+    .collection('movies')
+    .orderBy('createdAt')
+    .limit(ELEMENTS_ON_PAGE)
+
+  const pageMovies = await new Promise(
+    (resolve: (val: Movie[]) => void, reject) => {
+      page.get().then(snaps => {
+        if (snaps.docs.length > 0) {
+          const movies = []
+          snaps.forEach(movieDoc => {
+            movies.push({ id: movieDoc.id, ...movieDoc.data() })
+          })
+          // const last = snaps.docs[snaps.docs.length - 1]
+          resolve(movies)
+        }
+        resolve([])
+      })
+    }
+  )
+
+  return {
+    pageMovies,
+    ELEMENTS_ON_PAGE,
+  }
+}
+
+Home.getInitialProps = getProps()
 
 export default Home
